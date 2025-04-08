0bd5256907eb247faa296b4520598a1f4ad8251c
Luis <gerardo.fosado@cargamos.com>
2025-04-08 15:07:27 -0600

    locations services

--- pharmagob/v1/repository_interfaces/locations.py
+++ pharmagob/v1/repository_interfaces/locations.py
@@ -1,5 +1,17 @@
+from abc import abstractmethod
+from typing import Iterator, List, Optional, Tuple, Union
+
 from ._base import BaseRepositoryInterface
 
 
 class LocationRepositoryInterface(BaseRepositoryInterface):
-    pass
+    @abstractmethod
+    def get_by_umu_id(
+        self,
+        umu_id,
+        *,
+        sort: Optional[List[Tuple[str, int]]] = None,
+        projection: Optional[Union[list, dict]] = None,
+        limit: Optional[int] = None,
+    ) -> Tuple[int, Iterator[dict]]:
+        raise NotImplementedError
--- pharmagob/v1/services/locations.py
+++ pharmagob/v1/services/locations.py
@@ -1,3 +1,5 @@
+from typing import Iterator, Tuple
+
 from pharmagob.v1.models.location import LocationModel
 from pharmagob.v1.repository_interfaces.locations import LocationRepositoryInterface
 
@@ -6,3 +8,9 @@ from ._base import BaseService
 
 class LocationService(BaseService[LocationModel, LocationRepositoryInterface]):
     __model__ = LocationModel
+
+    def __init__(self, repository: LocationRepositoryInterface):
+        super().__init__(repository)
+
+    def get_by_umu_id(self, umu_id: str) -> Tuple[int, Iterator[dict]]:
+        return self.repository.get_by_umu_id(umu_id)
