6f221d013f909c718ac3955481ab82f0e2b85204
Luis <gerardo.fosado@cargamos.com>
2025-04-25 11:58:34 -0600

    updated

--- pharmagob/v1/services/doctors.py
+++ pharmagob/v1/services/doctors.py
@@ -1,3 +1,5 @@
+from typing import Iterator, Optional, Tuple
+
 from pharmagob.v1.models.doctor import DoctorModel
 from pharmagob.v1.repository_interfaces.doctors import DoctorRepositoryInterface
 
@@ -6,3 +8,23 @@ from ._base import BaseService
 
 class DoctorService(BaseService[DoctorModel, DoctorRepositoryInterface]):
     __model__ = DoctorModel
+
+    def search_by_employee_or_licence(
+        self,
+        employee_number: str,
+        *,
+        umu_id: str,
+        created_at_gt: int,
+        created_at_lt: int,
+        page: int,
+        limit: int,
+    ) -> Tuple[int, Iterator[DoctorModel]]:
+        count, result = self.repository.search_by_employee_or_licence(
+            employee_number,
+            created_at_gt=created_at_gt,
+            created_at_lt=created_at_lt,
+            page=page,
+            limit=limit,
+            umu_id=umu_id,
+        )
+        return count, map(lambda r: DoctorModel(**r), result)
