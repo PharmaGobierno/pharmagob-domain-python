792a674e227e06946ddb2adffcb5805aed5bbd20
Luis <gerardo.fosado@cargamos.com>
2025-04-30 12:24:41 -0600

    enums doctore

--- pharmagob/v1/models/doctor.py
+++ pharmagob/v1/models/doctor.py
@@ -9,6 +9,39 @@ class Status(str, Enum):
     ACTIVE = "ACTIVE"
     INACTIVE = "INACTIVE"
 
+class Specialty(str, Enum):
+    CARDIOLOGY = "CARDIOLOGY"
+    DERMATOLOGY = "DERMATOLOGY"
+    GYNECOLOGY = "GYNECOLOGY"
+    GENERAL_MEDICINE = "GENERAL_MEDICINE"
+    NEUROLOGY = "NEUROLOGY"
+    OPHTHALMOLOGY=  "OPHTHALMOLOGY"
+    ORTHOPEDICS = "ORTHOPEDICS"
+    OTORHINOLARYNGOLOGY = "OTORHINOLARYNGOLOGY"
+    PEDIATRICS = "PEDIATRICS"
+    PSYCHIATRY = "PSYCHIATRY"
+    UROLOGY = "UROLOGY"
+    ANESTHESIOLOGY = "ANESTHESIOLOGY"
+    DENTISTRY = "DENTISTRY"
+
+class Services(str, Enum):
+    SURGERY = "SURGERY"
+    OUTPATIENT_CONSULTATION = "OUTPATIENT_CONSULTATION"
+    HOSPITALIZATION = "HOSPITALIZATION"
+    PREVENTIVE_MEDICINE = "PREVENTIVE_MEDICINE"
+    REHABILITATION = "REHABILITATION"
+    EMERGENCY_CARE = "EMERGENCY_CARE"
+    LABORATORY = "LABORATORY"
+
+class Level(str, Enum):
+    JUNIOR = "JUNIOR"
+    SPECIALIST = "SPECIALIST"
+    RESIDENT = "RESIDENT"
+    SENIOR = "SENIOR"
+
+class Job_Position(str, Enum):
+    RESIDENT = "RESIDENT"
+
 
 @dataclass(kw_only=True)
 class DoctorModel(UpdatableModel):
@@ -20,11 +53,11 @@ class DoctorModel(UpdatableModel):
     last_name_2: Optional[str] = None
     employee_number: str
     profesional_licence: str
-    specialty: str
-    service: list[str]
+    specialty: Specialty
+    service: list[Services]
     status: Status
-    level: Optional[str] = None
-    job_position: Optional[str] = None
+    level: Optional[Level] = None
+    job_position: Optional[Job_Position] = None
 
     def __post_init__(self):
         super().__post_init__()
