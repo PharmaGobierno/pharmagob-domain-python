3fc39b88984a84e2f3d510ce33e884fb5935af75
Luis <gerardo.fosado@cargamos.com>
2025-04-30 13:31:35 -0600

    dispatch

--- pharmagob/v1/models/dispatch_record.py
+++ pharmagob/v1/models/dispatch_record.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from enum import Enum
 
 from typing import Optional
-from ._base import UpdatableModel
+from ._base import UpdatableModel, uuid_by_params
 from .minified import min_properties, min_models
 
 
@@ -30,3 +30,7 @@ class DispatchRecordModel(UpdatableModel):
     doctor: Optional[min_models.DoctorMin]
     patient: Optional[min_models.PatientMin]
     dispatch_details: list[min_properties.DispatchDetailMin]
+
+    def __post_init__(self):
+        super().__post_init__()
+        self._id = uuid_by_params(self.reference_id)
