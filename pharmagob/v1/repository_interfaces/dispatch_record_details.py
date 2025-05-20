07d170e7e6598ea3205c1ee64b3895b3a5593143
Ariel Villeda <ariel.villeda@cargamos.com>
2025-05-20 14:24:52 -0600

    get_by_dispatch_record_id

--- pharmagob/v1/repository_interfaces/dispatch_record_details.py
+++ pharmagob/v1/repository_interfaces/dispatch_record_details.py
@@ -1,5 +1,15 @@
+from abc import abstractmethod
+from typing import List, Optional, Tuple
+
 from ._base import BaseRepositoryInterface
 
 
 class DispatchRecordDetailRepositoryInterface(BaseRepositoryInterface):
-    pass
+    @abstractmethod
+    def get_by_dispatch_record_id(
+        self,
+        dispatch_record_id: str,
+        *,
+        umu_id: Optional[str] = None,
+    ) -> Tuple[int, List[dict]]:
+        raise NotImplementedError
