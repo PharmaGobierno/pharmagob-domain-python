f8514531204c21e3ebfae24b589e373b797de482
Ariel Villeda <ariel.villeda@cargamos.com>
2025-10-21 17:07:55 -0600

    .

--- pharmagob/v1/models/dispatch_record_status.py
+++ pharmagob/v1/models/dispatch_record_status.py
@@ -1,14 +1,8 @@
 from dataclasses import dataclass
-from enum import Enum
 from typing import Optional
 
 from ._base import BaseModel, uuid_by_params
 from .minified import min_models
-
-
-class QuantityActionType(str, Enum):
-    INCREASE = "INCREASE"
-    SUBTRACT = "SUBTRACT"
 
 
 @dataclass(kw_only=True)
