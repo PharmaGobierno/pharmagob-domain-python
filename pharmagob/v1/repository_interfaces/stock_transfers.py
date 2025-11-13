3986967c459bd36ac7e18a329e5a316edd293215
Ariel Villeda <ariel.villeda@cargamos.com>
2025-11-13 17:05:25 -0600

    stock_transfer.search_by_reference_id

--- pharmagob/v1/repository_interfaces/stock_transfers.py
+++ pharmagob/v1/repository_interfaces/stock_transfers.py
@@ -1,5 +1,22 @@
+from abc import abstractmethod
+from typing import Dict, List, Optional, Tuple
+
 from ._base import BaseRepositoryInterface
 
 
 class StockTransferRepositoryInterface(BaseRepositoryInterface):
-    pass
+    @abstractmethod
+    def search_by_reference_id(
+        self,
+        search_str: str,
+        *,
+        page: int = 1,
+        limit: int = 50,
+        sort: Optional[Dict[str, int]] = None,
+        last_event: Optional[str] = None,
+        umu_id: Optional[str] = None,
+        foreign_umu_id: Optional[str] = None,
+        created_at_gt: Optional[int] = None,
+        created_at_lt: Optional[int] = None,
+    ) -> Tuple[int, List[dict]]:
+        raise NotImplementedError
--- pharmagob/v1/services/stock_transfers.py
+++ pharmagob/v1/services/stock_transfers.py
@@ -1,3 +1,5 @@
+from typing import Dict, Iterator, Optional, Tuple
+
 from pharmagob.v1.models.stock_transfer import StockTransferModel
 from pharmagob.v1.repository_interfaces.stock_transfers import (
     StockTransferRepositoryInterface,
@@ -10,3 +12,29 @@ class StockTransferService(
     BaseService[StockTransferModel, StockTransferRepositoryInterface]
 ):
     __model__ = StockTransferModel
+
+    def search_by_reference_id(
+        self,
+        search_str: str,
+        *,
+        page: int = 1,
+        limit: int = 50,
+        sort: Optional[Dict[str, int]] = None,
+        last_event: Optional[str] = None,
+        umu_id: Optional[str] = None,
+        foreign_umu_id: Optional[str] = None,
+        created_at_gt: Optional[int] = None,
+        created_at_lt: Optional[int] = None,
+    ) -> Tuple[int, Iterator[StockTransferModel]]:
+        count, result = self.repository.search_by_reference_id(
+            search_str,
+            created_at_gt=created_at_gt,
+            created_at_lt=created_at_lt,
+            umu_id=umu_id,
+            last_event=last_event,
+            foreign_umu_id=foreign_umu_id,
+            page=page,
+            limit=limit,
+            sort=sort,
+        )
+        return count, map(lambda r: StockTransferModel(**r), result)
