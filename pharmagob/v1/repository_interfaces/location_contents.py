c296e96dd24459e29c1e6b033e7a7733775d716c
Ariel Villeda <ariel.villeda@cargamos.com>
2025-11-25 11:35:18 -0600

    new search function in location-contents

--- pharmagob/v1/repository_interfaces/location_contents.py
+++ pharmagob/v1/repository_interfaces/location_contents.py
@@ -21,3 +21,21 @@ class LocationContentRepositoryInterface(BaseRepositoryInterface):
         lot: Optional[str] = None,
     ) -> Tuple[int, List[dict]]:
         raise NotImplementedError
+
+    @abstractmethod
+    def search_by_item_global(
+        self,
+        search_str: str,
+        *,
+        page: int,
+        limit: int,
+        sort: Optional[Dict[str, int]] = None,
+        umu_id_in: Optional[List[str]] = None,
+        umu_id_not_in: Optional[List[str]] = None,
+        expiration_date_gt: Optional[int] = None,
+        expiration_date_lt: Optional[int] = None,
+        quantity_gt: Optional[int] = None,
+        quantity_lt: Optional[int] = None,
+        lot: Optional[str] = None,
+    ) -> Tuple[int, List[dict]]:
+        raise NotImplementedError
