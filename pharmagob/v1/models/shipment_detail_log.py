4001c4a89ab540c8cf965f30cf8ec62c0f0b3662
Alxhdz <alexis.delacruz@gruposid.com.mx>
2026-03-04 14:50:52 -0600

    Update shipment_detail_log.py

--- pharmagob/v1/models/shipment_detail_log.py
+++ pharmagob/v1/models/shipment_detail_log.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from ._base import BaseModel
 from .minified import min_models
+from .shipment import ReviewStatus
 
 
 @dataclass(kw_only=True)
@@ -10,6 +11,8 @@ class ShipmentDetailLogModel(BaseModel):
     __entity_name__ = "shipment-detail-logs"
 
     umu_id: str
+    shipment: min_models.ShipmentMin
+    review_status: ReviewStatus
     shipment_detail: dict
     origin_timestamp: int
     origin: str
